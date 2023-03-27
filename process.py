00f5159fc7bbc9956b381464025388c0ab5d822a
Andrés Arnedo Escalante <79600570+ArnedoAn@users.noreply.github.com>
2023-03-27 22:32:31 +0000

    third

--- process.py
+++ process.py
@@ -8,7 +8,7 @@ class Proceso:
         self.espera = None
 
     def __repr__(self):
-        return f"Proceso #{self.numero}: llegada={self.llegada}, cpu={self.cpu}, comienzo={self.comienzo}, final={self.final}, espera={self.espera}"
+        return f"Proceso #{self.numero}:" + "\n\t" + f"llegada={self.llegada}" + "\n\t" + f"cpu={self.cpu}" + "\n\t" + f"comienzo={self.comienzo}" + "\n\t" + f"final={self.final}" + "\n\t" + f"espera={self.espera}" + "\n"
 
 
 def sjf(procesos):
@@ -19,13 +19,17 @@ def sjf(procesos):
     procesosOrdenados = []
 
     while procesosRestantes > 0:
-        procesosDisponibles = [proceso for proceso in procesos if proceso.llegada <=
-                               tiempoActual and proceso not in procesosOrdenados]
+
+        procesosDisponibles = []
+        for proceso in procesos:
+            if proceso.llegada <= tiempoActual and proceso not in procesosOrdenados:
+                procesosDisponibles.append(proceso)
+
         if len(procesosDisponibles) == 0:
             tiempoActual += 1
             continue
-        procesoMasCorto = min(procesosDisponibles,
-                              key=lambda proceso: proceso.cpu)
+
+        procesoMasCorto = min(procesosDisponibles, key=lambda proceso: proceso.cpu)
         procesoMasCorto.comienzo = tiempoActual
         procesoMasCorto.final = tiempoActual + procesoMasCorto.cpu
         procesoMasCorto.espera = procesoMasCorto.comienzo - procesoMasCorto.llegada
